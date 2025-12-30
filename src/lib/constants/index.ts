13ea479ec8869e70a0eefbcc2de5cbf4afecb94a
Dreadz <vcs@dreadzverse.com>
2025-12-30 22:20:32 +0100

    Add app constant

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,6 @@
 export * from './activity-level.constant';
 export * from './age.constant';
+export * from './app.constant';
 export * from './height.constant';
 export * from './sex.constant';
 export * from './weight.constant';
