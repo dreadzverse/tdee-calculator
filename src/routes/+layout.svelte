4e81c2aa84b52f2bcc605482f08cac395c3e98ea
Dreadz <vcs@dreadzverse.com>
2025-12-30 22:21:40 +0100

    Set app name as page title in main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { APP_NAME } from '$lib/constants';
+
 	import './layout.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>{APP_NAME}</title>
+</svelte:head>
 
 {@render children()}
